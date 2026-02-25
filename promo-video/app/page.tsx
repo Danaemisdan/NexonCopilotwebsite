840d18a0167dd357849d36b2c0496124a7b89e5f
Danaemisdan <81348949+Danaemisdan@users.noreply.github.com>
2026-02-26 01:03:31 +0530

    fix: remove nested promo-video project to fix Vercel deployment

--- promo-video/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { CinematicSequence } from "@/components/cinematic-sequence";
-
-export default function PromoVideoPage() {
-  return (
-    <main className="bg-black min-h-screen">
-      <CinematicSequence />
-    </main>
-  );
-}
